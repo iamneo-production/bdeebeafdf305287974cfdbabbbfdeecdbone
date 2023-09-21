a268f22e54dc48409759c7ff0162c63c6f024ac7
Iamneo <techadmin@iamneo.ai>
2023-09-21 05:05:08 +0000

    Backup Commit

---
+++ que1.tf
@@ -0,0 +1,16 @@
+terraform {
+    required_providers {
+        aws = {
+            source = ""
+        }
+    }
+}
+provider "aws" {
+    region = "Singapore"
+    access_key = ""
+    secret_key = ""
+}
+resource "aws_instance" "linux_server" {
+    ami = "SUSE Linux Enterprise Server 12 SP5"
+    instance_type = "t2.micro"
+}
